b9ab57710c52153e1e6bb6949eff428527860151
Gabriel Prates <gabsprates@gmail.com>
2018-06-11 00:10:46 -0300

    templating and testing

--- __test__/Confirm.test.js
+++ __test__/Confirm.test.js
@@ -1,12 +1,35 @@
 import Confirm from '../src/Confirm';
 
 describe('Confirm', () => {
+  beforeEach(() => {
+    document.body.innerHTML = '';
+  });
+
   it('must to be a function', () => {
     expect(Confirm).toBeInstanceOf(Function);
   });
 
-  it('must to have the default state', () => {
-    const confirm = new Confirm();
-    expect(confirm.state).toBe(Confirm.defaultState);
+  describe('creating a new Confirm', () => {
+    let confirm;
+
+    beforeEach(() => {
+      confirm = new Confirm();
+    });
+
+    it('must to construct an instance of Confirm', () => {
+      expect(confirm).toBeInstanceOf(Confirm);
+    });
+
+    it('must to have the default state', () => {
+      expect(confirm.state).toBe(Confirm.defaultState);
+    });
+
+    it('must to append this.DOM on document.body', () => {
+      expect(document.body.firstChild).toBe(confirm.DOM);
+    });
+  });
+
+  it('musto to give the right remplate', () => {
+    expect(Confirm.template().box.outerHTML).toBe('<div class="cfrm"><h1></h1><div class="cfrm-msg"></div><div class="cfrm-btns"><button class="cfrm-btns-cancel"></button><button class="cfrm-btns-ok"></button></div></div>');
   });
 });
--- src/Confirm.js
+++ src/Confirm.js
@@ -1,6 +1,34 @@
 class Confirm {
   constructor() {
     this.state = Confirm.defaultState;
+    this.template = Confirm.template();
+    this.DOM = document.createElement('div');
+    document.body.appendChild(this.DOM);
+  }
+
+  static template() {
+    const box = document.createElement('div');
+    const title = document.createElement('h1');
+    const msg = document.createElement('div');
+    const btns = document.createElement('div');
+    const btnOk = document.createElement('button');
+    const btnCancel = document.createElement('button');
+
+    box.className = 'cfrm';
+    msg.className = 'cfrm-msg';
+    btns.className = 'cfrm-btns';
+    btnOk.className = 'cfrm-btns-ok';
+    btnCancel.className = 'cfrm-btns-cancel';
+
+    box.appendChild(title);
+    box.appendChild(msg);
+    box.appendChild(btns);
+    btns.appendChild(btnCancel);
+    btns.appendChild(btnOk);
+
+    return {
+      box, title, msg, btns, btnOk, btnCancel,
+    };
   }
 }
 
@@ -8,8 +36,7 @@ Confirm.defaultState = {
   title: '',
   isOpen: false,
   message: '',
-  confirmText: 'Ok',
-  onConfirm: () => {},
+  onConfirm: () => { },
 };
 
 export default Confirm;
